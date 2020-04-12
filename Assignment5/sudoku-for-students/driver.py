35753a585b2716f8312d954c90aab1c385de929d
Jinqi-Cheng <jessicaelf@hotmail.com>
2020-04-12 00:06:05 -0700

    updated driver.py

--- Assignment5/sudoku-for-students/driver.py
+++ Assignment5/sudoku-for-students/driver.py
@@ -3,30 +3,18 @@ from constraint_prop import AC3
 from csp_lib.backtrack_util import mrv, forward_checking, first_unassigned_variable, unordered_domain_values, no_inference
 from backtrack import backtracking_search
 
-print("================EASY1===============")
+for puzzle in [easy1, harder1]:
+    s = Sudoku(puzzle)  # construct a Sudoku problem
+    print("---initial state---")
+    s.display(s.infer_assignment())
+    print("\n")
+    AC3(s)
 
-s = Sudoku(easy1)  # construct a Sudoku problem
-print("---initial state---")
-s.display(s.infer_assignment())
-print("\n")
-AC3(s)
+    print("---after AC3---")
+    s.display(s.infer_assignment())
+    print("\n")
 
-print("---after AC3---")
-s.display(s.infer_assignment())
-print("\n")
-
-print("===============HARDER1==============")
-
-s = Sudoku(harder1)
-print("---initial state---")
-s.display(s.infer_assignment())
-print("\n")
-AC3(s)
-print("---after AC3---")
-s.display(s.infer_assignment())
-print("\n")
-
-if not s.goal_test(s.curr_domains):
-    solved = backtracking_search(s, select_unassigned_variable=mrv, inference=forward_checking)
-    print("---after backtracking---")
-    s.display(solved)
+    if not s.goal_test(s.curr_domains):
+        solved = backtracking_search(s, select_unassigned_variable=mrv, inference=forward_checking)
+        print("---after backtracking---")
+        s.display(solved)
